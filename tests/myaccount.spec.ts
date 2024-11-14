3d5a2a4f0967c7d2988590e6ddea028261497616
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-11-14 21:15:37 +0100

    Update myaccount.spec.ts

--- tests/myaccount.spec.ts
+++ tests/myaccount.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import exp from 'constants'
 
 
 test.describe('This page contains tests related to my account page', () => {
@@ -13,14 +14,17 @@ test.describe('This page contains tests related to my account page', () => {
         await expect(page).toHaveTitle(/account/)
     })
 
-  test('Scenario to test user registration', async ({ page }) => {
+    test('Scenario to test user registration', async ({ page }) => {
+        const username = 'er.animesh6789'
         // Identify the username field
-        await page.getByRole('textbox', { name: 'username' }).nth(1).fill('animesh66');
+        await page.getByRole('textbox', { name: 'username' }).nth(1).fill(username);
         // Identify the username field
-        await page.getByRole('textbox', { name: 'email' }).nth(1).fill('test@email.com');
+        await page.getByRole('textbox', { name: 'email' }).nth(1).fill('test6789@email.com');
         // Identify the password field
-        await page.getByRole('textbox', { name: 'password' }).nth(1).fill('AnimeshMukherjee@1234');
+        await page.getByRole('textbox', { name: 'password' }).nth(1).fill('AnimeshMukherjee@987651');
         // Identify Register button
         await page.getByRole('button', { name: 'register' }).click();
+        // Verify that the username should be displayed on the page
+        await expect(page.locator('.woocommerce-MyAccount-content')).toContainText(username);
     })
 })
