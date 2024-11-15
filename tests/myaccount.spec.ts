c29d4b6feb52ddad1569131fc151495bde14144f
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-11-15 20:27:45 +0100

    Update myaccount.spec.ts

--- tests/myaccount.spec.ts
+++ tests/myaccount.spec.ts
@@ -3,24 +3,19 @@ import { test, expect } from '@playwright/test'
 
 
 test.describe('This page contains tests related to my account page', () => {
+    const userName = 'er.animesh54564654';
+    const userEmail = `${userName}@email.com`;
 
     test.beforeEach(async ({ page }) => {
         // Open the url of the account
-        await page.goto('/my-account')
-    })
-
-    test('Verify that page title contains my account', async ({ page }) => {
+        await page.goto('/my-account');
         // Verify assertions
-        await expect(page).toHaveTitle(/account/)
-    })
-
-    test('Scenario to test user registration', async ({ page }) => {
-        const username = 'er.animesh7676876';
+        await expect(page).toHaveTitle(/account/);
         // Identify the username field
         await expect(page.getByRole('textbox', { name: 'username' }).nth(1)).toBeVisible();
-        await page.getByRole('textbox', { name: 'username' }).nth(1).pressSequentially(username, { delay: 100 });
+        await page.getByRole('textbox', { name: 'username' }).nth(1).pressSequentially(userName, { delay: 100 });
         // Identify the username field
-        await page.getByRole('textbox', { name: 'email' }).nth(1).pressSequentially('test7765675675@email.com', { delay: 100 });
+        await page.getByRole('textbox', { name: 'email' }).nth(1).pressSequentially(userEmail, { delay: 100 });
         // Identify the password field
         await expect(page.getByRole('textbox', { name: 'password' }).nth(1)).toBeVisible();
         await page.getByRole('textbox', { name: 'password' }).nth(1).pressSequentially('AnimeshMukherjee@987651', { delay: 100 });
@@ -29,7 +24,10 @@ test.describe('This page contains tests related to my account page', () => {
         await page.getByRole('button', { name: 'Register' }).click();
         //Verify that my account page has logged in
         await expect(page.getByRole('link', { name: /Dashboard/})).toBeVisible();
+    })
+
+    test('Scenario to test user registration for non register user', async ({ page }) => {
         // Verify that the username should be displayed on the page
-        await expect(page.locator('.woocommerce-MyAccount-content')).toContainText(username);
+        await expect(page.locator('.woocommerce-MyAccount-content')).toContainText(userName);
     })
 })
