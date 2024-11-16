1d90c975587a4931c722d6aea423d65bca1befa6
Animesh66 <er.animesh6@hotmail.com>
2024-11-16 17:10:40 +0100

    Refactored using page object model.

--- pages/basePage.page.ts
+++ pages/basePage.page.ts
@@ -1,11 +1,9 @@
-import { Locator, Page } from "@playwright/test";
+import { Page } from "@playwright/test";
 
 class BasePage {
-    userNameRegistration: Locator
 
     constructor(private _page: Page) {
         this._page = _page;
-
     };
 
 }
