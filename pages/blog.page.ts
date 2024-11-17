66a96856fefbcac62760cf3fedcaeb3238ac0d95
Animesh66 <er.animesh6@hotmail.com>
2024-11-17 18:15:10 +0100

    Added new tests for blog pages.

--- pages/blog.page.ts
+++ pages/blog.page.ts
@@ -2,11 +2,11 @@ import { Page, Locator } from '@playwright/test';
 import BasePage from "./basePage.page";
 
 class BlogPage extends BasePage {
-    recentPost: Locator
+    recentPostsList: Locator
 
     constructor(protected _page: Page) {
         super(_page);
-        this.recentPost = this._page.locator(".widget widget-sidebar-right");
+        this.recentPostsList = this._page.locator("#recent-posts-3 ul li");
 
     }
 
--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,12 +1,31 @@
 import { test, expect } from '@playwright/test'
+import BlogPage from '../pages/blog.page';
 
 
 test.describe('This file contains tests related to blog page', () => {
+    let blog: BlogPage;
 
-    test('Verify the title of about page', async ({ page }) => {
-        // Navigate to about page
-        await page.goto('/about');
-        // Verify title of the about page
-        await expect(page).toHaveTitle(/About/);
+    test.beforeEach(async ({ page }) => {
+        // Initalize the object for blog page
+        blog = new BlogPage(page);
+        // Navigate to blog page
+        await blog.navigatePage('/blog');
+        // Verify title of the blog page
+        await expect(page).toHaveTitle(/Blog/);
     })
+
+    test('Verify recent posts count must be equals to 5', async () => {
+        // Get all the elements of recent post containing multiple elements and count() will
+        // return the count of elements in the item
+        expect(await blog.recentPostsList.count()).toEqual(5);
+
+    });
+
+    test('Verify each recent post must have at least 10 charater for headign', async () => {
+        // Loop over individual elements of the recent post and get the count and verisy the lengths
+        for (let post of await blog.recentPostsList.elementHandles()) {
+            let postHeading = await post.textContent();
+            expect(postHeading?.trim().length).toBeGreaterThan(15);
+        }
+    });
 })
