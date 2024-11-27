4c05ecbce374cd1b039a2fb66e61a7e025ab721a
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-11-27 16:04:29 +0100

    Update shop.spec.ts

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -8,28 +8,28 @@ let filterOptions = [{ filterValue: 'popularity' },
 { filterValue: 'price' },
 { filterValue: 'price-desc' }];
 
-filterOptions.forEach(({ filterValue }) => {
-    test.describe('This file contains tests related to shop page', () => {
+
+    test.describe('This file contain tests related to shop page', () => {
         let shop: ShopPage;
+        filterOptions.forEach(({ filterValue }) => {
+            test.beforeEach('Open shop page and verify tittle', async ({ page }) => {
+                shop = new ShopPage(page);
+                await test.step('SetUp: Open shop page and verify title', async () => {
+                    // Navigate to shop page
+                    Logger.info("Navigating to shop page.");
+                    await shop.navigatePage('/shop');
+                    // Verify page title
+                    Logger.info("Verify page title have shop");
+                    await expect(page).toHaveTitle(/Shop/);
+                });
 
-        test.beforeEach('Open shop page and verify tittle', async ({ page }) => {
-            shop = new ShopPage(page);
-            await test.step('SetUp: Open shop page and verify title', async () => {
-                // Navigate to shop page
-                Logger.info("Navigating to shop page.");
-                await shop.navigatePage('/shop');
-                // Verify page title
-                Logger.info("Verify page title have shop");
-                await expect(page).toHaveTitle(/Shop/);
             });
 
-        })
-
-        test(`Verify the sorting dropdown can be changed based on ${filterValue} @sanity`, async () => {
-            await test.step(`Step: Select filter value ${filterValue}`, async () => {
-                Logger.info(`Selecting filter value ${filterValue} in shop page`);
-                await shop.VerifySortBy(filterValue);
-            });
+            test(`Verify the sorting dropdown can be changed based on ${filterValue} @sanity`, async () => {
+                await test.step(`Step: Select filter value ${filterValue}`, async () => {
+                    Logger.info(`Selecting filter value ${filterValue} in shop page`);
+                    await shop.VerifySortBy(filterValue);
+                });
 
         });
     });
